4eddbc876d9e13e09fa9a2a7e365a3fab3ec6ac6
BustDot <Bust.dev@outlook.com>
2025-01-13 11:08:34 +0800

    test: add basic benchmark

---
+++ benchmark/bench_test.go
@@ -0,0 +1,59 @@
+package benchmark
+
+import (
+	"bitcask"
+	"bitcask/utils"
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var db *bitcask.DB
+
+func init() {
+	var err error
+	options := bitcask.DefaultOptions
+	options.DirPath = "/tmp/bitcask-go-bench"
+	db, err = bitcask.Open(options)
+	if err != nil {
+		panic(fmt.Sprintf("failed to open db: %v", err))
+	}
+}
+
+func Benchmark_Put(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		err := db.Put(utils.GetTestKey(i), utils.RandomValue(1024))
+		assert.Nil(b, err)
+	}
+}
+
+func Benchmark_Get(b *testing.B) {
+	for i := 0; i < 100000; i++ {
+		err := db.Put(utils.GetTestKey(i), utils.RandomValue(1024))
+		assert.Nil(b, err)
+	}
+
+	rand.Seed(time.Now().Unix())
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		_, err := db.Get(utils.GetTestKey(rand.Int()))
+		if err != nil && !errors.Is(err, bitcask.ErrKeyNotFound) {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_Delete(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		err := db.Delete(utils.GetTestKey(rand.Int()))
+		assert.Nil(b, err)
+	}
+}
