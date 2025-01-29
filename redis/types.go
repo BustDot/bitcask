69fc3564c5f57cc7b44d226f5d0b2a655624ad8f
BustDot <Bust.dev@outlook.com>
2025-01-29 17:32:37 -0500

    feat: support for redis List

--- redis/types.go
+++ redis/types.go
@@ -296,3 +296,98 @@ func (rds *RedisDataStructure) SRem(key []byte, member []byte) (bool, error) {
 
 	return true, nil
 }
+
+// ======================== List ========================
+
+func (rds *RedisDataStructure) pushInner(key []byte, element []byte, isLeft bool) (uint32, error) {
+	meta, err := rds.findMetadata(key, List)
+	if err != nil {
+		return 0, err
+	}
+
+	// Construct listInternalKey
+	lk := &listInternalKey{
+		key:     key,
+		version: meta.version,
+	}
+	if isLeft {
+		lk.index = meta.head - 1
+	} else {
+		lk.index = meta.tail
+	}
+
+	wb := rds.db.NewWriteBatch(bitcask.DefaultWriteBatchOptions)
+	encKey := lk.encode()
+	_ = wb.Put(encKey, element)
+	meta.size++
+	if isLeft {
+		meta.head--
+	} else {
+		meta.tail++
+	}
+	_ = wb.Put(key, meta.encode())
+	if err = wb.Commit(); err != nil {
+		return 0, err
+	}
+
+	return meta.size, nil
+}
+
+func (rds *RedisDataStructure) LPush(key []byte, element []byte) (uint32, error) {
+	return rds.pushInner(key, element, true)
+}
+
+func (rds *RedisDataStructure) RPush(key []byte, element []byte) (uint32, error) {
+	return rds.pushInner(key, element, false)
+}
+
+func (rds *RedisDataStructure) popInner(key []byte, isLeft bool) ([]byte, error) {
+	meta, err := rds.findMetadata(key, List)
+	if err != nil {
+		return nil, err
+	}
+
+	if meta.size == 0 {
+		return nil, nil
+	}
+
+	// Construct listInternalKey
+	lk := &listInternalKey{
+		key:     key,
+		version: meta.version,
+	}
+	if isLeft {
+		lk.index = meta.head
+	} else {
+		lk.index = meta.tail - 1
+	}
+
+	encKey := lk.encode()
+	value, err := rds.db.Get(encKey)
+	if err != nil {
+		return nil, err
+	}
+
+	wb := rds.db.NewWriteBatch(bitcask.DefaultWriteBatchOptions)
+	_ = wb.Delete(encKey)
+	meta.size--
+	if isLeft {
+		meta.head++
+	} else {
+		meta.tail--
+	}
+	_ = wb.Put(key, meta.encode())
+	if err = wb.Commit(); err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+func (rds *RedisDataStructure) LPop(key []byte) ([]byte, error) {
+	return rds.popInner(key, true)
+}
+
+func (rds *RedisDataStructure) RPop(key []byte) ([]byte, error) {
+	return rds.popInner(key, false)
+}
